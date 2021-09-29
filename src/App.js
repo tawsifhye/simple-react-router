6189ec06ae364f2076665db87a6fb52ef135f436
tawsifhye <tawsif.lucse@gmail.com>
2021-09-29 13:46:28 +0600

    static routing added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Friends from './components/Friends/Friends';
@@ -9,7 +9,23 @@ import NotFound from './NotFound/NotFound';
 function App() {
   return (
     <div className="App">
-      <Friends></Friends>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/home'></Route>
+          <Route path='/friends'>
+            <Friends></Friends>
+          </Route>
+          <Route path='/about'>
+            <About></About>
+          </Route>
+          <Route path='*'>
+            <NotFound> </NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
