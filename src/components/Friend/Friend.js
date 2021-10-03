b2578bf6df3e7d2a44f03b66401c422d67c07c84
tawsifhye <tawsif.lucse@gmail.com>
2021-10-03 22:13:45 +0600

    navigate fixed or dynamic route using a button click

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
     const {id, name, phone, website, address} = props.friend;
+    const history = useHistory();
     const friendstyle ={
         border:'3px solid goldenrod',
         padding: '10px',
         borderRadius: '10px'
+    }
+
+    const handleFriendClick = () =>{
+        history.push(`/friend/${id}`)
     }
     return (
         <div style={friendstyle}>
@@ -15,6 +20,8 @@ const Friend = (props) => {
             <p>Visit me: {website}</p>
             <p><small>City: {address.city}</small></p>
             <Link to={`/friend/${id}`}>Visit Me</Link>
+            <br />
+            <button onClick={handleFriendClick}>Visit Me</button>
         </div>
     );
 };
